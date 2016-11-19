c500a94e41b06d879355b2395a9381da6e7bc1c5
Atta Ho <attaho17@gmail.com>
2016-11-19 20:19:42 +0800

    Fixed the self view for characters

---
+++ Assets/Prototype Space/Brian/Scripts/SelfViewController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class SelfViewController : NetworkBehaviour {
+
+	public MeshRenderer mr;
+
+	// Use this for initialization
+	void Start () {
+		if (!this.IsOwnByLocalPlayer()) {
+			mr.enabled = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
