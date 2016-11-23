238247e234447ceeac5f6f7a71cd6874cd25c3f6
Brian So <briansostudio@gmail.com>
2016-11-23 23:09:54 +0800

    map generation improvement

--- Assets/Master/Map/StartingPointStyler.cs
+++ Assets/Master/Map/StartingPointStyler.cs
@@ -4,7 +4,11 @@ using System.Collections;
 public class StartingPointStyler : MapGenerator.MapGenerationMiddleware {
     protected override void Process(){
         
-        // This is not necessary true, but just a fast code 
+        // This is not necessary true, but just a fast code
+        map.blocks[2,2].tileNumber = MapGenerator.TILE_GLASS;
+        map.blocks[map.width - 3,2].tileNumber = MapGenerator.TILE_GLASS;
+        map.blocks[2,map.height - 3].tileNumber = MapGenerator.TILE_GLASS;
+        map.blocks[map.width - 3,map.height - 3].tileNumber = MapGenerator.TILE_GLASS;
         map.startingPoints.Add(new Vector2(2,2));
         map.startingPoints.Add(new Vector2(map.width - 3,2));
         map.startingPoints.Add(new Vector2(2,map.height - 3));
